e61b79240a5181b659b578b47f980d521fdbf1f2
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2024-12-25 17:43:00 +0500

    serializer push

--- core/settings.py
+++ core/settings.py
@@ -182,4 +182,29 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "https://dtm.dtpi-dictionary.uz",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.0.1:8000",
+    "https://dtm.dtpi-dictionary.uz"
+]
+
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS"
+]
+CORS_ALLOW_HEADERS = [
+    "Authorization",
+    "Content-Type",
+    "Accept",
+    "X-CSRFToken"
+]
+CORS_ALLOW_CREDENTIALS = True
