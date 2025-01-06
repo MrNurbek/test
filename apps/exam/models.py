f22745f0f515e0898ae0e8d72a51f129c802b874
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2025-01-06 16:05:00 +0500

    serializer push

--- apps/exam/models.py
+++ apps/exam/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from apps.category.models import Category
-from apps.testbase.models import  Topic
-
-
+from apps.testbase.models import Topic
 
 
 class Exam(models.Model):
@@ -11,9 +9,7 @@ class Exam(models.Model):
     start_time = models.DateTimeField(null=True, blank=True)
     end_time = models.DateTimeField(null=True, blank=True)
     total_questions = models.IntegerField()
-    time_limit = models.IntegerField(help_text="Time limit for the exam in minutes", null=True, blank=True)
+    time_limit = models.TimeField(help_text="Time limit for the exam in HH:MM:SS format", null=True, blank=True)
 
     def __str__(self):
         return f"Exam: {self.category.name} ({self.start_time} - {self.end_time} - id={self.id})"
-
-
