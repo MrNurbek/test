bef8b480c82b2b22a75a9bb90468cf67380fe9c4
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2025-01-06 16:33:54 +0500

    serializer push

--- api/exam/serializers.py
+++ api/exam/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from apps.category.models import Category
 from apps.exam.models import Exam
 from apps.testbase.models import Topic
 
@@ -8,17 +9,22 @@ from apps.testbase.models import Topic
 
 
 class ExamSerializer(serializers.ModelSerializer):
-    category = serializers.CharField(source='category.name', read_only=True)
-    topics = serializers.SerializerMethodField()
-    start_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', input_formats=['%Y-%m-%d %H:%M:%S'])
-    end_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', input_formats=['%Y-%m-%d %H:%M:%S'])
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    topics = serializers.PrimaryKeyRelatedField(
+        queryset=Topic.objects.all(), many=True
+    )
+    start_time = serializers.DateTimeField(
+        format='%Y-%m-%d %H:%M:%S', input_formats=['%Y-%m-%d %H:%M:%S']
+    )
+    end_time = serializers.DateTimeField(
+        format='%Y-%m-%d %H:%M:%S', input_formats=['%Y-%m-%d %H:%M:%S']
+    )
 
     class Meta:
         model = Exam
         fields = ['id', 'category', 'topics', 'start_time', 'end_time', 'total_questions', 'time_limit']
-
-    def get_topics(self, obj):
-        return [topic.name for topic in obj.topics.all()]
 
     def create(self, validated_data):
         topics = validated_data.pop('topics', [])
