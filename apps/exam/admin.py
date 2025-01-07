7f13998628163882bc249e6a7ed4e64d9971ab4a
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2025-01-07 14:15:31 +0500

    serializer push

--- apps/exam/admin.py
+++ apps/exam/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from apps.exam.models import Exam
+from datetime import timedelta
 
 
 
 
 
+@admin.register(Exam)
+class ExamAdmin(admin.ModelAdmin):
+    list_display = ('category', 'start_time', 'end_time', 'total_questions', 'time_limit')
+    fields = ('category', 'topics', 'start_time', 'end_time', 'total_questions', 'time_limit')
 
-
-
-admin.site.register(Exam)
+    def save_model(self, request, obj, form, change):
+        if isinstance(obj.time_limit, timedelta):
+            obj.time_limit = obj.time_limit
+        else:
+            obj.time_limit = timedelta(minutes=30)
+        super().save_model(request, obj, form, change)
