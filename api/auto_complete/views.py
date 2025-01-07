63fac88f9ed6fc250acca65b4fdc94371bd857f4
MrNurbek1223 <xamrayevnurbek00@gmail.com>
2025-01-07 14:37:59 +0500

    serializer push

--- api/auto_complete/views.py
+++ api/auto_complete/views.py
@@ -42,11 +42,15 @@ def auto_complete_exams():
     for exam_attempt in expired_attempts:
         time_limit = exam_attempt.user_exam.exam.time_limit
 
-        # Agar time_limit allaqachon timedelta bo'lsa
+        # To'g'ri turdagi qiymat ekanligini tekshiramiz
         if isinstance(time_limit, timedelta):
             expiration_time = exam_attempt.started_at + time_limit
         elif isinstance(time_limit, (int, float)):
             expiration_time = exam_attempt.started_at + timedelta(minutes=time_limit)
+        elif isinstance(time_limit, str):
+            # HH:MM:SS formatidagi qiymatni timedelta ga aylantirish
+            h, m, s = map(int, time_limit.split(':'))
+            expiration_time = exam_attempt.started_at + timedelta(hours=h, minutes=m, seconds=s)
         else:
             raise TypeError(f"Unsupported time_limit type: {type(time_limit)}")
 
